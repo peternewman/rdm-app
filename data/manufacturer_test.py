0897b21bdeb64261fa02a22a3910426fa6c3d7c0
Peter Newman <peterjnewman@gmail.com>
2026-07-05 13:22:12 +0100

    Fix a syntax error

--- data/manufacturer_test.py
+++ data/manufacturer_test.py
@@ -142,7 +142,7 @@ class TestManufacturers(unittest.TestCase):
         if hasattr(e, 'errno'):
           self.fail("Link %s failed due to socket error %s" % (link, e.errno))
       else:
-        self.assertTrue(((response.code == 200) || (response.code == 202)),
+        self.assertTrue(((response.code == 200) or (response.code == 202)),
                          "Failed to fetch URL %s got status %d" %
                          (link, response.code))
 
