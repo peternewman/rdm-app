cb11fcf5e4b555817ce7a22920bff4c50bcb4d2b
Peter Newman <peterjnewman@gmail.com>
2026-07-05 13:35:29 +0100

    https://www.productionwarehouse.co.za/ is a different type of timeout error on CI

--- data/manufacturer_test.py
+++ data/manufacturer_test.py
@@ -133,9 +133,10 @@ class TestManufacturers(unittest.TestCase):
                     link == 'https://www.acuitybrands.com/' or
                     link == 'https://www.nxp.com/' or
                     link == 'https://www.martin.com/' or
-                    link == 'https://www.productionwarehouse.co.za/' or
                     link == 'https://portmanlights.com/' or
-                    link == 'https://www.adamhall.com/'))):
+                    link == 'https://www.adamhall.com/')) or
+                  (type(e) is URLError and
+                   (link == 'https://www.productionwarehouse.co.za/'))):
             self.fail("Link %s failed due to %s, reason type: %s" % (link, e.reason, type(e)))
         elif hasattr(e, 'code'):
           self.fail("The server couldn't fulfill the request for %s. Error "
