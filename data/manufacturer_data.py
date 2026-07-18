b5539379454312f8246a588192a337ec4fa9a30f
Peter Newman <peterjnewman@gmail.com>
2026-07-18 17:27:03 +0100

    Autoupdated manufacturer data Please check this manually before merging

--- data/manufacturer_data.py
+++ data/manufacturer_data.py
@@ -1359,6 +1359,7 @@ MANUFACTURER_DATA = [
   (0x4253, "Barco"),
   (0x42A2, "Birket Engineering, Inc."),
   (0x4321, "Design Partners of Canada"),
+  (0x432A, "432audio"),
   (0x4344, "CDCA Ltd."),
   (0x4347, "CAST Software"),
   (0x4349, "C.I.Tronics Lighting Designers Ltda"),
