ce50bdca25e2b404c228edba184b28f3faf70c16
Peter Newman <peternewman@users.noreply.github.com>
2026-09-14 20:54:45 +0100

    Correct the error type we're skipping offstagecontrols against

--- data/manufacturer_test.py
+++ data/manufacturer_test.py
@@ -134,10 +134,10 @@ class TestManufacturers(unittest.TestCase):
                     link == 'https://www.nxp.com/' or
                     link == 'https://www.martin.com/' or
                     link == 'https://portmanlights.com/' or
-                    link == 'https://www.adamhall.com/' or
-                    link == 'https://www.offstagecontrols.com/')) or
+                    link == 'https://www.adamhall.com/')) or
                   (type(e) is URLError and
-                   (link == 'https://www.productionwarehouse.co.za/'))):
+                   (link == 'https://www.productionwarehouse.co.za/' or
+                    link == 'https://www.offstagecontrols.com/'))):
             self.fail("Link %s failed due to %s, reason type: %s" % (link, e.reason, type(e)))
         elif hasattr(e, 'code'):
           self.fail("The server couldn't fulfill the request for %s. Error "
